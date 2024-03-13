b74989f67387cfa65353d4cd16a1c7a6126760fe
Nia <108984073+kawaii-Code@users.noreply.github.com>
2024-03-13 23:07:08 +0300

    Удалил скрипты 📄

--- TestMod/postbuild.bat
+++
@@ -1,1 +0,0 @@
-xcopy /Y "bin\Debug\TestMod.dll" "C:\Program Files (x86)\Steam\steamapps\common\Valheim\BepInEx\plugins\TestMod.dll"
--- TestMod/prebuild.bat
+++
@@ -1,4 +0,0 @@
-tasklist | find /i "valheim.exe" && taskkill /im valheim.exe /F
-
-xcopy /Y "C:\DATA\VALHEIM\ValheimRipped\Assets\AssetBundles" "AssetBundles"
-DEL "AssetBundles\*.meta" "AssetBundles\*.manifest"
